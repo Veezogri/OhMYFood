1187a3c6115346dc371d697533ccc711c141b523
Fares RIAL <rialfares@gmail.com>
2025-01-29 11:55:59 +0100

    opti css + opti hmtl + ajout js coeur rempli en click

--- ___ohmyfood-Greta/___ohmyfood-Greta/Briefing_OpenClassrooms/script.js
+++ ___ohmyfood-Greta/___ohmyfood-Greta/Briefing_OpenClassrooms/script.js
@@ -1,9 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
     const likeButtons = document.querySelectorAll('.card__like');
 
     likeButtons.forEach(button => {
-        button.addEventListener('click', () => {
+        button.addEventListener('click', (e) => {
+            e.preventDefault();
+            button.classList.toggle('fas');
             button.classList.toggle('filled');
+            
+
+            
+
+            
         });
     });
-});
+
+
+
+
